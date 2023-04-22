51ab1d10a5ab4335a66fc9ff6db9f85134b307f5
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2023-04-22 00:17:02 -0600

    GifService class created and tested

--- bloc_app/package/gif_service/lib/src/gif_service.dart
+++ bloc_app/package/gif_service/lib/src/gif_service.dart
@@ -1,7 +1,45 @@
 /// {@template gif_service}
 /// My Gif Service
 /// {@endtemplate}
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
 class GifService {
   /// {@macro gif_service}
-  const GifService();
+  GifService({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  final http.Client _httpClient;
+  final String baseUrl = "api.giphy.com";
+  final String endPoint = "/v1/gifs/trending";
+  final String apiKey = "0grOe9fa9N4iBT9iaQgGP28B7qoD2zZ8";
+
+  Future<List<String>> fetchGifs() async {
+    final uri = Uri.http(baseUrl, endPoint, {'api_key': apiKey});
+
+    http.Response response;
+    List body;
+    try {
+      response = await _httpClient.get(uri);
+    } on Exception {
+      throw Exception();
+    }
+    if (response.statusCode != 200) {
+      throw HttpRequestException();
+    }
+    try {
+      body = jsonDecode(response.body)['data'] as List;
+    } on Exception {
+      throw JsonDecodeException();
+    }
+    return body
+        .map((url) => url['images']['original']['url'].toString())
+        .toList();
+  }
 }
+
+class HttpRequestException implements Exception {}
+
+class JsonDecodeException implements Exception {}
